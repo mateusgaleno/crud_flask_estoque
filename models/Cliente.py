9e901b1552e6a280afe0f85942834c8e42b86f4b
mateusgaleno <mateusgaleno.al@gmail.com>
2023-06-24 13:07:41 -0300

    Create Cliente.py
    models cliente

---
+++ models/Cliente.py
@@ -0,0 +1,13 @@
+class Cliente:
+    def __init__(self,id,nome, idade, profissao):
+        self.id = id
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
+
+class id:
+    def __init__(self,id,nome, idade, profissao):
+        self.id = id
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
